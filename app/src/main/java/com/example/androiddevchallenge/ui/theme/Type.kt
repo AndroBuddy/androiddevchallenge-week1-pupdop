e9eddb72b5e3039bb447ad331ff60802cf5dd128
AndroBuddy <anirudhchimpidi8@gmail.com>
2021-02-26 22:27:04 +0530

    Set RedHatDisplay as app font

--- app/src/main/java/com/example/androiddevchallenge/ui/theme/Type.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/theme/Type.kt
@@ -17,27 +17,87 @@ package com.example.androiddevchallenge.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.androiddevchallenge.R
+
+private val appFontFamily = FontFamily(
+    fonts = listOf(
+        Font(
+            resId = R.font.redhatdisplay_black,
+            weight = FontWeight.Black,
+            style = FontStyle.Normal
+        ),
+        Font(
+            resId = R.font.redhatdisplay_blackitalic,
+            weight = FontWeight.Black,
+            style = FontStyle.Italic
+        ),
+        Font(
+            resId = R.font.redhatdisplay_bold,
+            weight = FontWeight.Bold,
+            style = FontStyle.Normal
+        ),
+        Font(
+            resId = R.font.redhatdisplay_bolditalic,
+            weight = FontWeight.Bold,
+            style = FontStyle.Italic
+        ),
+        Font(
+            resId = R.font.redhatdisplay_italic,
+            weight = FontWeight.Normal,
+            style = FontStyle.Italic
+        ),
+        Font(
+            resId = R.font.redhatdisplay_regular,
+            weight = FontWeight.Normal,
+            style = FontStyle.Normal
+        ),
+        Font(
+            resId = R.font.redhatdisplay_medium,
+            weight = FontWeight.Medium,
+            style = FontStyle.Normal
+        ),
+        Font(
+            resId = R.font.redhatdisplay_mediumitalic,
+            weight = FontWeight.Medium,
+            style = FontStyle.Italic
+        )
+    )
+)
+
+private val defaultTypography = Typography()
 
 // Set of Material typography styles to start with
 val typography = Typography(
-    body1 = TextStyle(
-        fontFamily = FontFamily.Default,
+    h1 = defaultTypography.h1.copy(
+        fontFamily = appFontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 24.sp,
+        letterSpacing = 0.15.sp
+    ),
+    h2 = defaultTypography.h2.copy(
+        fontFamily = appFontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 16.sp,
+        letterSpacing = 0.15.sp
+    ),
+    h3 = defaultTypography.h3.copy(fontFamily = appFontFamily),
+    h4 = defaultTypography.h4.copy(fontFamily = appFontFamily),
+    h5 = defaultTypography.h5.copy(fontFamily = appFontFamily),
+    h6 = defaultTypography.h6.copy(fontFamily = appFontFamily),
+    subtitle1 = defaultTypography.subtitle1.copy(fontFamily = appFontFamily),
+    subtitle2 = defaultTypography.subtitle2.copy(fontFamily = appFontFamily),
+    body1 = defaultTypography.body1.copy(
+        fontFamily = appFontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp
-    )
-        /* Other default text styles to override
-    button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
-        fontSize = 14.sp
     ),
-    caption = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
-    )
-    */
+    body2 = defaultTypography.body2.copy(fontFamily = appFontFamily),
+    button = defaultTypography.button.copy(fontFamily = appFontFamily),
+    caption = defaultTypography.caption.copy(fontFamily = appFontFamily),
+    overline = defaultTypography.overline.copy(fontFamily = appFontFamily)
 )
